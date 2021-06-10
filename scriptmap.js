050e2f510e49969a760de034cff43793d7f57465
ваш юзернейм на гитхабе <alexa.z0t0va@gmail.com>
2021-06-10 23:11:41 +0300

    add map html

---
+++ scriptmap.js
@@ -0,0 +1,8 @@
+var box = document.querySelector(".box");
+for (let index = 0; index < array.length; index++) {
+    var elemnt = document.createElement"div")
+    element.classList.add('item')
+    element.innerHTML = index;
+    box.appendChild(element);
+    
+}
